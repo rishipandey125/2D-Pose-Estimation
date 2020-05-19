93a62972390dabc926d7df5bd08dd5e55384859e
rishipandey125 <rpandey2@illinois.edu>
2020-05-19 15:09:37 -0500

    Configured the CNN
    configured the cnn with my schema, working on testing it now

--- mocap.py
+++ mocap.py
@@ -3,9 +3,8 @@ import cv2
 #first goal is to identify pose and draw skeletal mesh on img
 
 # Paths for the CNN (off of my git)
-#it cant read the prototxt file
-protoFile = "Desktop/code/pose_estimation_model/pose_deploy_linevec_faster_4_stages.prototxt"
-weightsFile = "Desktop/code/pose_estimation_model/pose_iter_160000.caffemodel"
+protoFile = "/Users/rishipandey125/Desktop/code/pose_estimation_model/pose_deploy_linevec_faster_4_stages.prototxt.txt"
+weightsFile = "/Users/rishipandey125/Desktop/code/pose_estimation_model/pose_iter_160000.caffemodel"
 
 # Reading the CNN
 net = cv2.dnn.readNetFromCaffe(protoFile,weightsFile)
