9184560d7c2d237ef13bd1b28701412d130496e9
rishipandey125 <rpandey2@illinois.edu>
2020-05-19 13:51:47 -0500

    integrating the cnn
    integrating the pose cnn model into the file schema. The network is on my local machine.

--- mocap.py
+++ mocap.py
@@ -1,3 +1,9 @@
 import cv2
 #simple mocap project created by Rishi Pandey
 #first goal is to identify pose and draw skeletal mesh on img
+
+# Paths for the CNN (off of my git)
+protoFile = "Desktop/code/pose_estimation_model/pose_deploy_linevec_faster_4_stages.prototxt"
+weightsFile = "Desktop/code/pose_estimation_model/pose_iter_160000.caffemodel"
+# Reading the CNN
+network = cv2.dnn.readNetFromCaffe(protoFile,weightsFile)
