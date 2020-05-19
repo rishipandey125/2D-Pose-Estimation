edf46245d0b450e0b66c92a5011f8dabea4a524c
rishipandey125 <rpandey2@illinois.edu>
2020-05-19 14:43:16 -0500

    working on integrating cnn

--- mocap.py
+++ mocap.py
@@ -3,7 +3,9 @@ import cv2
 #first goal is to identify pose and draw skeletal mesh on img
 
 # Paths for the CNN (off of my git)
+#it cant read the prototxt file
 protoFile = "Desktop/code/pose_estimation_model/pose_deploy_linevec_faster_4_stages.prototxt"
 weightsFile = "Desktop/code/pose_estimation_model/pose_iter_160000.caffemodel"
+
 # Reading the CNN
-network = cv2.dnn.readNetFromCaffe(protoFile,weightsFile)
+net = cv2.dnn.readNetFromCaffe(protoFile,weightsFile)
