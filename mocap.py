5b41598b6405debcc901bbf5490e50447cbf2dca
rishipandey125 <rpandey2@illinois.edu>
2020-05-22 12:54:16 -0500

    notes on the confidence map
    wrote some notes of understanding the confidence map and 4d matrix so I can understand how cv works in general and move forward

--- mocap.py
+++ mocap.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 #simple mocap project created by Rishi Pandey
 #first goal is to identify pose and draw skeletal mesh on img
 
@@ -27,10 +28,25 @@ output = network.forward()
 
 '''
 first dimension is the image id
+
 second dimension is the index of the keypoint (which number point is it)
-although we have like 18 points it will be out of 44 because past the first 18 is a heatmap
+and its confidence map
+although we have like 15 points it will be out of 44 because past the first 15 is a heatmap
+What does the confidence map mean? The confidence map can be accessed as output[0,x,:,:]
+The confidence map for every keypoint x where (x in numKeyPoints) is the "heat map" around that keypointself.
+The max in that confidence map is the specific point on that heatmap that is the most precise point.
+For example if we are identifying the knee cap. A heat map will be drawn around the general location.
+We want the most precise location so we would find the global maxima of that heatmap. Think of it as a plotting of a 3d
+function over a joint. You want to find that maxima and its location to know the precise location of the knee cap.
+
 third dimension is the height of the output map (where it is +y?)
 fourth dimension is the width of the output map (where it is +x?)
 '''
-for x in output[0]:
-    print(x)
+
+keyPoints = []
+numKeyPoints = 15
+
+for x in range(numKeyPoints):
+    confidenceMap = output[0,x,:,:]
+    print(confidenceMap) #this produces a confidence map
+    print("---------")
