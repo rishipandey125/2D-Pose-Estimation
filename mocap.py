acca41d9f875d17006af823e4b63ca5e1b08cd25
rishipandey125 <rpandey2@illinois.edu>
2020-05-28 13:04:22 -0500

    writing the keyPoints data analysis func

--- mocap.py
+++ mocap.py
@@ -5,100 +5,103 @@ import numpy as np
 #rework and smooth that data mathematically
 #write the final videos with the smoothed data
 
-'''
-Current Improvements: Joint Swapping, No Overlap, Missing Joint Estimation
-Savgol Smoothing?
-'''
-# Paths for the CNN (off of my git)
-protoFile = "/Users/rishipandey125/Desktop/code/pose_estimation_model/pose_deploy_linevec_faster_4_stages.prototxt.txt"
-weightsFile = "/Users/rishipandey125/Desktop/code/pose_estimation_model/pose_iter_160000.caffemodel"
-
-# Reading the CNN
-network = cv2.dnn.readNetFromCaffe(protoFile,weightsFile)
-
 #Path to Video File
 videoPath = "test_images/serge2.mp4"
 video = cv2.VideoCapture(videoPath)
-#boolean stating there is a next frame, and storing the next frame in the variable frame
-hasFrame,frame = video.read()
-#write output video
-outputVideo = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
 
-skeletonPairs = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13]]
-correspondingJoints = [[2,5],[3,6],[4,7],[8,11],[9,12],[10,13]]
+def analyzeKeyPoints(video):
+    # Paths for the CNN (on local machine)
+    protoFile = "/Users/rishipandey125/Desktop/code/pose_estimation_model/pose_deploy_linevec_faster_4_stages.prototxt.txt"
+    weightsFile = "/Users/rishipandey125/Desktop/code/pose_estimation_model/pose_iter_160000.caffemodel"
 
-#loop through the below with each video frame, write that drawn skeleton to the outputVideo
-count = 0
-while hasFrame:
-    img = frame
-    imgHeight = img.shape[0]
-    imgWidth = img.shape[1]
-    #Prep Input Image for Network
-    inWidth = 368
-    inHeight = 368
-    inpBlob = cv2.dnn.blobFromImage(img, 1.0 / 255, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False)
-    network.setInput(inpBlob)
+    # Reading the CNN
+    network = cv2.dnn.readNetFromCaffe(protoFile,weightsFile)
 
-    #Output Matrix of CNN given the input Image
-    output = network.forward()
-    #height and width of output
-    height = output.shape[2]
-    width = output.shape[3]
+    #boolean stating there is a next frame, and storing the next frame in the variable frame
+    hasFrame,frame = video.read()
+    #corresponding joints data for swapping
+    correspondingJoints = [[2,5],[3,6],[4,7],[8,11],[9,12],[10,13]]
+    while hasFrame:
+        imgHeight = frame.shape[0]
+        imgWidth = frame.shape[1]
+        #Prep Input Image for Network
+        inWidth = 368
+        inHeight = 368
+        inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False)
+        network.setInput(inpBlob)
 
-    #keyPoints is a list of the locations of the 15 points, main data needed for writing to animation
-    keyPoints = []
-    numKeyPoints = 15
-    corresponding_index = 0
-    previous_cord = None
-    for i in range(numKeyPoints):
-        confidenceMap = output[0,i,:,:]
-        #only using prob and point
-        minVal, prob, minLoc, point = cv2.minMaxLoc(confidenceMap)
-        #KeyPoint in Threshold
-        if prob > 0.1:
-            #check 5,6,7 and 11,12,13 for joint swapping
-            #scale x and y values
-            x = int((imgWidth*point[0])/width)
-            y = int((imgHeight*point[1])/height)
-            keyPoints.append((x,y))
-            previous_cord = (x,y)
-            #index out of bounds here
-            if corresponding_index < len(correspondingJoints):
-                if i == correspondingJoints[corresponding_index][1]:
-                    previousPoint = correspondingJoints[corresponding_index][0]
-                    corresponding_index += 1
-                    if keyPoints[previousPoint][0] > keyPoints[i][0]:
-                        #swap
-                        tempPoint = keyPoints[i]
-                        keyPoints[i] = keyPoints[previousPoint]
-                        keyPoints[previousPoint] = tempPoint
+        #Output Matrix of CNN given the input Image
+        output = network.forward()
 
-        elif previous_cord != None:
-            keyPoints.append(previous_cord)
-            previous_cord = None
-        else:
-            keyPoints.append(None)
-    #collect the data in a frame: [x cords] [ycords] format where xcords and ycords are ALWAYS of length 15
+        #height and width of output
+        height = output.shape[2]
+        width = output.shape[3]
+
+        keyPoints = []
+        numKeyPoints = 15
+        corresponding_index = 0
+        for i in range(numKeyPoints):
+            x_keyPoints = []
+            y_keyPoints = []
+            confidenceMap = output[0,i,:,:]
+            #only using prob and point
+            minVal, prob, minLoc, point = cv2.minMaxLoc(confidenceMap)
+            #KeyPoint in Threshold or just grab data for the first frame
+            if prob > 0.1 or len(keyPoints) == 0:
+                #scale x and y values
+                x = int((imgWidth*point[0])/width)
+                y = int((imgHeight*point[1])/height)
+                x_keyPoints.append(x)
+                y_keyPoints.append(y)
+                if corresponding_index < len(correspondingJoints):
+                    if i == correspondingJoints[corresponding_index][1]:
+                        previousPoint = correspondingJoints[corresponding_index][0]
+                        corresponding_index += 1
+                        if keyPoints[previousPoint][0] > keyPoints[i][0]: #swap!
+                            tempPoint = keyPoints[i]
+                            keyPoints[i] = keyPoints[previousPoint]
+                            keyPoints[previousPoint] = tempPoint
+
+            else:
+                x_keyPoints.append(previous_x[i])
+                y_keyPoints.append(previous_y[i])
+        # updating frame for next iteration
+        previous_x, previous_y = x_keyPoints, y_keyPoints
+        keyPoints.append(x_keyPoints + y_keyPoints)
+        hasFrame,frame = video.read()
+
+    return keyPoints
+
+
+
+
+
+
+
+
+
+
+# skeletonPairs = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13]]
+
+    #collect the data in a frame: [x cords] + [ycords] format where xcords and ycords are ALWAYS of length 15
+    #then the structure will be frame num: [all cords] where all cords is on length 30 and the first 15 are x, and the last 15 are y
+    #then you can smoooth each row
     #there can be no None in this list
     # draw skeleton
-    for pair in skeletonPairs:
-        point1 = pair[0]
-        point2 = pair[1]
-        if keyPoints[point1] != None and keyPoints[point2] != None:
-            #draws a line between the two corresponding points
-            cv2.circle(img, keyPoints[point1], 20, (255, 0, 0), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(img, "{}".format(point1), keyPoints[point1], cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, lineType=cv2.LINE_AA)
-            cv2.circle(img, keyPoints[point2], 20, (255, 0, 0), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(img, "{}".format(point2), keyPoints[point2], cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, lineType=cv2.LINE_AA)
-            cv2.line(img, keyPoints[point1], keyPoints[point2], (0, 0, 255), 10)
-    outputVideo.write(img)
+    # for pair in skeletonPairs:
+    #     point1 = pair[0]
+    #     point2 = pair[1]
+    #     if keyPoints[point1] != None and keyPoints[point2] != None:
+    #         #draws a line between the two corresponding points
+    #         cv2.circle(img, keyPoints[point1], 20, (255, 0, 0), thickness=-1, lineType=cv2.FILLED)
+    #         cv2.putText(img, "{}".format(point1), keyPoints[point1], cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, lineType=cv2.LINE_AA)
+    #         cv2.circle(img, keyPoints[point2], 20, (255, 0, 0), thickness=-1, lineType=cv2.FILLED)
+    #         cv2.putText(img, "{}".format(point2), keyPoints[point2], cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, lineType=cv2.LINE_AA)
+    #         cv2.line(img, keyPoints[point1], keyPoints[point2], (0, 0, 255), 10)
+    # outputVideo.write(img)
     # updating frame for next iteration
     # print("Write Frame")
-    count += 1
-    if count == 50:
-        break
-    hasFrame,frame = video.read()
 
 
 # print("DONE WRITING LOOP EXITED")
-outputVideo.release()
+# outputVideo.release()
